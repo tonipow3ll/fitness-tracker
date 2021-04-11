7e7b776f01eee269c083347b8ed8e25bc3c047ec
Toni Powell <tonipow3ll@gmail.com>
2021-04-11 13:16:42 -0400

    html routes working

--- controller/htmlroutes.js
+++ controller/htmlroutes.js
@@ -3,7 +3,15 @@ const express = require('express');
 const path = require('path')
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
 module.exports = router;
