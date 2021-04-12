29860b331f143bf1574c92c54447721366b96879
Toni Powell <tonipow3ll@gmail.com>
2021-04-12 15:29:44 -0400

    hmmmmmmm

--- controller/apiroutes.js
+++ controller/apiroutes.js
@@ -1,8 +1,21 @@
 const { response } = require('express');
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const { Exercise } = require('../models');
+// const exercise = require('../models/Exercise');
+// const workout = require('../models/Workout');
+const db = require('../models');
 const router = express.Router();
 
+router.get('/api/exercise', (req, res) => {
+    db.Workout.find({}, (err, workouts) => {
+        try {
+            res.json(Exercise)
+        } catch (err) {
+            res.json(err)
+        }
+    })
+})
 // router.get('/exercise', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 // });
