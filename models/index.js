22175c36c234e5b2d5578d5eaa2fd64efbaaa5bd
Toni Powell <tonipow3ll@gmail.com>
2021-04-15 09:13:55 -0400

    wompwomp

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-module.exports = {
-    Exercise: require('./Exercise'),
-    Workout: require('./Workout')
-}
+// module.exports = {
+//     Exercise: require('./Exercise'),
+//     Workout: require('./Workout')
+// }
