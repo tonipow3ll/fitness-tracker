a6063ef7c3f678237bfc55d26b107d67f30c18f6
Toni Powell <tonipow3ll@gmail.com>
2021-04-17 14:31:24 -0400

    fixing routes

--- controller/api.js
+++ controller/api.js
@@ -8,30 +8,21 @@ const router = express.Router();
 // get route for all exercises
 router.get('/api/workouts/range', (req, res) => {
     db.Workout.find({})
-    .then((dbWorkouts) =>{
-        console.log(dbWorkouts)
-        res.json(dbWorkouts)
-    })
+        .then((dbWorkouts) => {
+            console.log(dbWorkouts)
+            res.json(dbWorkouts)
+        })
 });
 // get route for all workouts
 router.get('/api/workouts', (req, res) => {
-          db.Workout.find({})
-    .then((dbWorkouts) =>{
-        console.log(dbWorkouts)
-        res.json(dbWorkouts)
-    })
+    db.Workout.find({})
+        .then((dbWorkouts) => {
+            console.log(dbWorkouts)
+            res.json(dbWorkouts)
+        })
 })
 
-router.post('/exercise', (req, res) => {
-    db.Workout.insertOne({
-        type: req.body.type,
-        name: req.body.name,
-        duration: req.body.duration,
-        weight: req.body.weight,
-        reps: rep.body.reps,
-        sets: req.body.sets
-    })
-});
+
 
 router.post('/workouts', (req, res) => {
     db.Workout.insertOne({
@@ -44,27 +35,17 @@ router.post('/workouts', (req, res) => {
     })
 });
 
-router.put('/workouts', (req, res) => {
-    db.Workout.insertOne({
-        type: req.body.type,
-        name: req.body.name,
-        duration: req.body.duration,
-        weight: req.body.weight,
-        reps: rep.body.reps,
-        sets: req.body.sets
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(
+        exerciseId,
+        { $push: { exercises: exerciseInfo } },
+        { new: true }
+    ).then((chosenWorkout) => {
+        res.json(chosenWorkout)
     })
 });
 
-router.put('/exercise', (req, res) => {
-    db.Workout.insertOne({
-        type: req.body.type,
-        name: req.body.name,
-        duration: req.body.duration,
-        weight: req.body.weight,
-        reps: rep.body.reps,
-        sets: req.body.sets
-    })
-});
+
 
 
 
